c76b7a239a71f4a1a15dc42816b30af73c5b236b
Vitaly <selivanov@ub.edu>
2016-07-25 16:31:45 +0200

    initial commit

---
+++ lib.R
@@ -0,0 +1,155 @@
+binom<-function(n){ #calculation of binomial cefficients
+ bf<-numeric(n); bf[1]<-1;
+ if(n>1){ for(i in 2:n) bf[i]<-bf[i-1]*i;  bc<-numeric(n); 
+   for(i in 1:(n-1)) bc[i]=bf[n]/bf[i]/bf[n-i];}
+   else  bc<-1;
+     bc }
+
+carbons<-function(n, bc) {#natural distribution of 13C according to the number of carbons in whole fragment
+  pc12<-0.989; pc13<-0.011;
+    mc<-numeric(n+1); mc[1]<-pc12^n;
+ if(n>1) {for(i in 1:(n-1)) mc[i+1]<-bc[i]*pc12^(n-i)*pc13^i; mc[n+1]<-pc13^n;}
+   else {if(n==1) mc[2]<-pc13;}
+       mc }
+    
+silicium<-function(mc){#correction of natural mass distribution accounting for Si
+  pSi0<-0.9223; pSi1<-0.0467; pSi2<-0.031;
+   n<-length(mc)
+    m<-numeric(n+2)
+     m[1]<-mc[1]*pSi0;
+     m[2]<-mc[2]*pSi0+mc[1]*pSi1;
+  for(i in 3:n) m[i]<-mc[i]*pSi0+mc[i-1]*pSi1+mc[i-2]*pSi2;
+     m[n+1]<-mc[n]*pSi1+mc[n-1]*pSi2;
+       m[n+2]<-mc[n]*pSi2;
+         m }
+      
+sulfur<-function(mc){#correction of natural mass distribution accounting for S
+  pS0<-0.9504; pS1<-0.0075; pS2<-0.0421;
+   n<-length(mc)
+    m<-numeric(n+2)
+     m[1]<-mc[1]*pS0;
+     m[2]<-mc[2]*pS0+mc[1]*pS1;
+  for(i in 3:n) m[i]<-mc[i]*pS0+mc[i-1]*pS1+mc[i-2]*pS2;
+     m[n+1]<-mc[n]*pS1+mc[n-1]*pS2;
+       m[n+2]<-mc[n]*pS2;
+         m }
+      
+tdist<-function(nc,nsi,ns){ #final natural mass distribution
+   bcc<-binom(nc);
+    m<-carbons(nc, bcc);
+ if(nsi>0) for(i in 1:nsi)  m<-silicium(m);
+ if(ns>0) for(i in 1:ns)  m<-sulfur(m);
+      m }
+ 
+norm<-function(msd,f1){#normalization of GCMS data accounting for the loss of protons
+        colon<-length(msd);# f1<-f1[1];
+    for (i in 3:(colon-1)) msd[i]<-(msd[i]+msd[i]*f1-msd[i+1]*f1);
+   scol<- msd[3]; for(i in 4:(colon-1)) scol<-scol+msd[i];
+  msd[2]<-msd[2]*0; 
+ for (i in 3:(colon-1)) msd[i]<-(msd[i]/scol); #normalization
+  scol<- msd[3]; for(i in 4:(colon-1)) scol<-scol+msd[i];#=1
+    msd[colon]<-scol; 
+      msd }
+   
+mtr<-function(nfrg,nmass,nC,nSi,nS){ #various numbers of labeled 13C with tails of natural distributions
+    mt<-tdist(nC,nSi,nS); 
+     lem<-length(mt); if(lem<nmass) for(i in (lem+1):nmass) mt[i]<-0.0;
+      mt<-mt[1:nmass];
+       mt<-mt/sum(mt);
+        mm<-numeric(nmass*(nfrg+1));          
+         attr(mm,"dim")<-c(nfrg+1,nmass);
+          mm[1,]<-mt;
+       for(i in 1:nfrg) { mt<-tdist(nC-i,nSi,nS); lem<-length(mt);
+         if(lem<(nmass-i)) {for(k in (lem+1):(nmass-i)) mt[k]<-0.0;}
+             mt<-mt[1:(nmass-i)];  mt<-mt/sum(mt);
+               for(j in 1:length(mt)) mm[i+1,j+i]<-mt[j];}
+              mm}
+              
+mdistr<-function(nreal,msd,mm,nln){ #label incorporation             
+          fr<-msd; colon<-length(fr); for(i in 2:colon) fr[i]<-0;
+ for(j in 1:(nreal+1)) {fr[j+1]<-msd[j+2]/mm[j,j];
+   for(i in j:(colon-2)) msd[i+2]<-msd[i+2]-fr[j+1]*mm[j,i];}
+# normalization:
+  fr[colon]=fr[2];
+        for(i in 3:(colon-2)) {fr[colon]<- fr[colon]+fr[i];}
+#     for(i in 2:(colon-2)) fr[i]<- fr[i]/fr[colon];
+      fr }
+
+stat<-function(dfr,nln,nfrg){            i<-1; lfr<-length(dfr);
+  dfr1=data.frame();
+  while(i<nln){ cnlab<-substr(as.character(dfr[i,1]),4,9); k<-i;
+    while(substr(as.character(dfr[k+1,1]),4,9)==cnlab) k<-k+1; 
+      if(k>i) {sredn<-dfr[i,];
+                 for(j in 2:lfr) sredn[j]<-mean(dfr[i:k,j]);
+                 for(j in 2:lfr) if(sredn[j]<0.) sredn[j]<-0.;
+#                 srsum<-sum(sredn[2:(2+nfrg)]);
+#               sredn[2:(2+nfrg)]<-sredn[2:(2+nfrg)]/srsum;
+               otkl<-dfr[1,]; otkl[1]<-as.factor("**sd**");
+                 for(j in 2:length(otkl)) otkl[j]<-sd(dfr[i:k,j]);
+        dfr1<-rbind(dfr1,sredn,otkl);
+                  i<-k;}
+       else{dfr1=rbind(dfr1,dfr[i,]);}
+          i<-i+1;}
+          return(list(dfr1,nln))
+} 
+
+suminj<-function(dfr,nln,colon){         i<-1;
+  while(i<nln-1){ cnlab<-substr(as.character(dfr[i,1]),1,3); 
+    while(substr(as.character(dfr[i+1,1]),1,3)==cnlab) {
+      dfr[i,2:colon]<-dfr[i,2:colon]+dfr[i+1,2:colon];
+        dfr<-dfr[-(i+1),]; nln<-nln-1;}
+             i<-i+1;}
+             return(list(dfr,nln))
+}
+remsd<-function(mid,lbl){ numl=nrow(mid);
+    k=1
+        for(i in 1:(numl)){
+         if(as.character(mid[k,1])==lbl) {mid=mid[-k,]}
+          else {k=k+1}
+        }
+    return(mid)
+}
+chirow<-function(dfr,draw,nmi,dlfrg,dteor){     len=length(draw);
+        mid=numeric(nmi); tmp<-draw;
+         for(j in 1:(dlfrg+1)) mid=mid+dfr[1,j+1]*dteor[j,];
+          tmp[2:(2+dlfrg)]= ((draw[2:(2+dlfrg)]-mid[1:(1+dlfrg)])/0.005)^2;
+          tmp[len]=sqrt(sum(tmp[2:(2+dlfrg)]));
+    return(tmp)
+}
+
+alchi<-function(dfr,draw,nmi,dlfrg,dteor){     tmp<-draw; numl=nrow(draw);
+            for(i in 1:(numl)){ tmp[i,]=chirow(dfr[i,],draw[i,],nmi,dlfrg,dteor)};
+    return(tmp)
+}
+
+chandis<-function(mdis,ic,fac){
+  mdis[ic]=fac*mdis[ic];  len=length(mdis); suma=0;
+    for(i in 2:len) suma=suma+mdis[i];
+    for(i in 2:len) mdis[i]=mdis[i]/suma;  
+   return(mdis)
+}
+
+ci99<-function(mid,rada,nadi,nfra,fac,nmass,nfrg,chi){
+      len=length(rada)
+     while(chi<6.63){ if(mid[nfra]>0.999){break}; if(mid[nfra]<0.001){break}
+     mid=chandis(mid,nfra,fac);
+     tmp=chirow(mid,rada,nmass,nfrg,nadi); chi=tmp[len]
+     cat("\n chi: ",as.character(format(tmp,digits=4)),"\n")
+     cat(" mid: ",as.character(format(mid,digits=4)),"\n")
+     }
+       return(list(mid,chi))
+}
+
+ch2par<-function(mid,pinc,pdec,inc,rada,nadi,nmass,nfrg,chi0){
+  cat("\n pink=",pinc," pdec=",pdec)
+     chi=chi0; len=length(rada); mid0=mid
+     while(chi<(chi0+0.00001)){mid0=mid; chi0=chi;
+      if(mid[pinc]>0.99){break}; if(mid[pdec]<0.005){break}
+     mid[pinc]=mid[pinc]+inc; mid[pdec]=mid[pdec]-inc;
+     tmp=chirow(mid,rada,nmass,nfrg,nadi); chi=tmp[len];
+     cat("\n chi: ",as.character(format(tmp,digits=4)),"\n")
+     cat(" mid: ",as.character(format(mid,digits=4)),"\n")
+     }
+       return(list(mid0,chi0))
+}
+
---
+++ midcor.R
@@ -0,0 +1,74 @@
+
+correct<-function(fname,mdcor="con"){
+  md<-substr(mdcor,1,2);
+# read experimental data
+ fn<-file.path(fname);
+  info<-read.table(fn, nrows=4);  nC<-info$V2[1]; nfrg<-info$V2[2]; nSi<-info$V2[3]; nS<-info$V2[4];
+      gcms<-read.table(fn, skip=5);
+           coln<-length(gcms);  nln<-nrow(gcms); nmass<-coln-2;
+# theoretic distribution:
+          mmlab<-mtr(nfrg,nmass,nC,nSi,nS);
+# normalization
+    ef<-sum(gcms[2])/sum(gcms[3]);
+         gcmsn<-norm(gcms,ef);
+# mass fractions
+   fr<-mdistr(nfrg,gcmsn,mmlab,nln);# write mass fractions without correction:
+ fn1<-paste(fn,"_c",sep="");
+ write("*** MID for each injection, corrected only for natural 13C, 29,30Si, 33,34S ***",fn1)
+  write.table(format(fr,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F);
+# Sum the data for various injections from the same plate:
+  lst=suminj(gcms,nln,coln); gcms=lst[[1]]; nln=lst[[2]];
+# normalization
+         gcmsn<-norm(gcms,ef);
+# mass fractions
+      fr<-mdistr(nfrg,gcmsn,mmlab,nln);
+ write("\n*** Summed injections for each plate, corrected only for natural 13C, 29,30Si, 33,34S **",fn1,append=TRUE)
+  write.table(format(fr,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F);
+# correction
+           corr<-numeric(nmass);
+       corr<-gcmsn[1,3:(2+nmass)]-mmlab[1,];
+  if(md=="va") { for(ii in 1:9) {tmp<-gcmsn;  
+#    for(j in 1:1)  for(k in 1:(nfrg+1))  for(i in 1:(nmass+1-k)) tmp[j,i+1+k]<-tmp[j,i+1+k]-corr[i]*(fr[j,1+k]);
+    for(j in 1:nln) { k=1; for(i in 1:(nmass+1-k)) tmp[j,i+1+k]<-tmp[j,i+1+k]-corr[i]*(fr[j,1+k]);
+               k=2; for(i in 1:(nmass+1-k)) tmp[j,i+1+k]<-tmp[j,i+1+k]-corr[i]*(fr[j,1+k])*0.573;#
+               k=3; for(i in 1:(nmass+1-k)) tmp[j,i+1+k]<-tmp[j,i+1+k]-corr[i]*(fr[j,1+k])*0.;#
+               for(k in 4:(nfrg+1)) for(i in 1:(nmass+1-k)) tmp[j,i+1+k]<-tmp[j,i+1+k]-corr[i]*(fr[j,1+k])*0.62;#
+    }
+      fr<-mdistr(nfrg,tmp,mmlab,nln);
+        }}  else {tmp<-gcmsn; for(j in 1:nln)  tmp[j,3:(2+nmass)]<-tmp[j,3:(2+nmass)]-corr;
+     fr<-mdistr(nfrg,tmp,mmlab,nln);}
+# statistics in row data     
+  lst=stat(tmp,nln,nfrg); gcmsn2=lst[[1]]; gcmsn2$V2=NULL; 
+ write("\n*** Summed injections for each plate corrected for peaks overlap, statistics **",fn1,append=TRUE)
+  write.table(format(gcmsn2,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F);
+#     statistics in final distribution of 13C isotopomers
+lst=stat(fr,nln,nfrg); fr=lst[[1]]; nln=lst[[2]]; len=length(fr)-1;
+# write data:
+       razn<-gcmsn[1,];  razn[1]<-as.factor("correction"); for(i in 1:nmass) razn[i+1]<-corr[i];
+       fr<-fr[1:len]
+ write("\n*** Statistics, samples fully corrected **",fn1,append=TRUE)
+    write.table(format(fr,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F); 
+ write("*** Correction factor: **",fn1,append=TRUE)
+     write.table(format(razn,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F);
+   frmod=remsd(fr,"**sd**")
+   gcmsn2=remsd(gcmsn2,"**sd**")
+     gcms=alchi(frmod,gcmsn2,nmass,nfrg,mmlab);
+ write("\n*** chi: **",fn1,append=TRUE)
+     write.table(format(gcms,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F);
+     mid=frmod[2,]; chi=0; nma=which(mid==max(mid[2:len])); fac=0.98;
+     rada=gcmsn2[2,]
+     for(i in 1:3){
+   lst=ci99(mid,rada,mmlab,nma,fac,nmass,nfrg,chi); mid=lst[[1]]; chi=lst[[2]]
+   lst=ch2par(mid,4,2,0.001,rada,mmlab,nmass,nfrg,chi); mid=lst[[1]]; chi=lst[[2]]
+   lst=ch2par(mid,6,4,0.001,rada,mmlab,nmass,nfrg,chi); mid=lst[[1]]; chi=lst[[2]]
+   lst=ch2par(mid,6,2,0.001,rada,mmlab,nmass,nfrg,chi); mid=lst[[1]]; chi=lst[[2]]
+   lst=ch2par(mid,6,5,0.001,rada,mmlab,nmass,nfrg,chi); mid=lst[[1]]; chi=lst[[2]]
+   lst=ch2par(mid,4,5,0.001,rada,mmlab,nmass,nfrg,chi); mid=lst[[1]]; chi=lst[[2]]
+   lst=ch2par(mid,5,2,0.001,rada,mmlab,nmass,nfrg,chi); mid=lst[[1]]; chi=lst[[2]]
+     }
+     razn=mid; razn[2:len]=mid[2:len]-frmod[2,2:len];
+     cat(" diff: ",as.character(format(razn,digits=4)),"\n")
+     
+     cat("\n diff: ",as.character(format(razn,digits=4)),"\n",file=fn1,append=TRUE)
+     cat(" mid: ",as.character(format(mid,digits=4)),"\n",file=fn1,append=TRUE)
+}
