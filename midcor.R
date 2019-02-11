cb74a1ea518ce26008b0ac41af95c4c0ffc9a1a3
seliv55 <selivanov@ub.edu>
2019-02-11 15:23:00 +0100

    midcor changed

--- midcor.R
+++ midcor.R
@@ -26,8 +26,8 @@ fitf<-function(tmp,mmlab,corr,ff,fr,nfrg){ nln=nrow(tmp); nmass=ncol(tmp)-2;
        return(list(ff,xisq,fr))
 }
 
-correct<-function(fname,samb=1,samf=3,cndb=4,cndf=9){#fname is the name of file with raw data;
-# samb,samf,cndb,cndf are the positions of the initial and final characters in the row name
+correct<-function(fname,injb=11,injf=12,plab=4,plaf=9){#fname is the name of file with raw data;
+# injb,injf,plab,plaf are the positions of the initial and final characters in the row name
 # designating the biological sample and conditions correspondingly
  fn<-file.path(fname);# read experimental data
   info<-read.table(fn, nrows=4);  nC<-info$V2[1]; nfrg<-info$V2[2]; nSi<-info$V2[3]; nS<-info$V2[4];
@@ -44,7 +44,7 @@ correct<-function(fname,samb=1,samf=3,cndb=4,cndf=9){#fname is the name of file 
  write("*** MID for each injection, corrected only for natural 13C, 29,30Si, 33,34S ***",fn1)
   write.table(format(fr,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F);
 # Sum the data for various injections from the same plate:
-  lst=suminj(gcms,nln,coln,samb,samf); gcms=lst[[1]]; nln=lst[[2]];
+  lst<-suminj(gcms,nln-1,coln,injb,injf); gcms=lst[[1]]; nln=lst[[2]];
 # normalization
          gcmsn<-norm(gcms,ef);
 # mass fractions
@@ -53,7 +53,7 @@ correct<-function(fname,samb=1,samf=3,cndb=4,cndf=9){#fname is the name of file 
   write.table(format(fr,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F);
 # correction
            corr<-numeric(nmass); corr1=numeric(nmass); icomm=0;
-           patt=grep('comm',fr[,1]);
+           patt=grep('Cold',fr[,1]);
       if(length(patt)>0) corr<-gcmsn[patt[1],3:(2+nmass)]-mmlab[1,]; print(corr)
            patt=grep('natur',fr[,1]);
       if(length(patt)>0) corr1<-gcmsn[patt[1],3:(2+nmass)]-mmlab[1,];
@@ -67,11 +67,11 @@ correct<-function(fname,samb=1,samf=3,cndb=4,cndf=9){#fname is the name of file 
         }}  else {tmp<-gcmsn; for(j in 1:nln)  tmp[j,3:(2+nmass)]<-tmp[j,3:(2+nmass)]-corr;
      fr<-mdistr(nfrg,tmp,mmlab,nln);}
 # statistics in row data     
-  lst=stat(tmp,nln,nfrg,cndb,cndf); gcmsn2=lst[[1]]; gcmsn2$V2=NULL; 
+  lst=stat(tmp,nln,nfrg,plab,plaf); gcmsn2=lst[[1]]; gcmsn2$V2=NULL; 
 # write("\n*** Summed injections, statistics for raw data corrected for peaks overlap **",fn1,append=TRUE)
 #  write.table(format(gcmsn2,digits=4),fn1,quote=FALSE,append=TRUE,col.names=FALSE, row.names = F);
 #     statistics in final distribution of 13C isotopomers
-lst=stat(fr,nln,nfrg,4,9); fr=lst[[1]]; nln=lst[[2]]; len=length(fr)-1;
+lst=stat(fr,nln,nfrg,plab,plaf); fr=lst[[1]]; nln=lst[[2]]; len=length(fr)-1; #CF changed lst=stat(fr,nln,nfrg,4,9); fr=lst[[1]]; nln=lst[[2]]; len=length(fr)-1;
 # write data:
        razn<-gcmsn[1,];  razn[1]<-as.factor("correction"); for(i in 1:nmass) razn[i+1]<-corr[i];
        fr<-fr[1:len]
@@ -87,7 +87,7 @@ lst=stat(fr,nln,nfrg,4,9); fr=lst[[1]]; nln=lst[[2]]; len=length(fr)-1;
  write("\n*** 99% CI: **",fn1,append=TRUE)
      
      mid=frmod[2,];  fac=0.98; fac1=0.9;     rada=gcmsn2[2,]
-  confin(mid,rada,fac,fac1,mmlab,nmass,nfrg,11,fn1)
-     mid=frmod[3,];  rada=gcmsn2[3,]
-  confin(mid,rada,fac,fac1,mmlab,nmass,nfrg,11,fn1)
+  #confin(mid,rada,fac,fac1,mmlab,nmass,nfrg,11,fn1)
+  #   mid=frmod[3,];  rada=gcmsn2[3,]
+  #confin(mid,rada,fac,fac1,mmlab,nmass,nfrg,11,fn1)
 }
